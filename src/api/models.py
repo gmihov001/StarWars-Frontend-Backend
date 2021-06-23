fecc64ff455465f3eeb2e93f6e19ddd6a2ef6314
George Mihov <gmihov001@gmail.com>
2021-06-23 21:27:44 +0000

    Updates

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -20,9 +19,10 @@ class User(db.Model):
 
 class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    entity_type = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    entity_id = db.Column(db.Integer, unique=True, nullable=False)
+    url = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<Favorite %r>' % self.name
@@ -30,7 +30,10 @@ class Favorites(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "entity_type": self.entity_type,
             "name": self.name,
+            "entity_id": self.entity_id,
+            "url": self.url,
             # do not serialize the password, its a security breach
         }
               
