a099f6e020f04a4a0135fc286e6310dd2d1f80b7
George Mihov <gmihov001@gmail.com>
2021-06-28 16:12:12 +0000

    Implemented serialization for  db.relationship columns as nested lists

--- src/api/models.py
+++ src/api/models.py
@@ -9,17 +9,51 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     favorites = db.relationship('Favorite', backref='user')
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
+
+    # def serialize(self):
+    #     return {
+    #         "id": self.id,
+    #         "username": self.username,
+    #         "email": self.email,
+    #         "favorites": self.favorites,
+    #         # do not serialize the password, its a security breach
+    #     }
+
+    # # def serialize(self):
+    # #     if self.favorites:
+    # #         favorites = [upload.serialize_upload_bis() for upload in self.favorites]
+    # #     return {
+    # #             "id": self.id,
+    # #             "username": self.username,
+    # #             "email": self.email,
+    # #             "favorites": favorites,
+    # #     }
+
+    # # Upload serializer
+    # def serialize_upload_bis(self):
+    #     return {
+    #         "name": self.name,
+    #         "path_to_file": self.path_to_file,
+    #     }
+
+    # def serialize_upload(self):
+    #     if self.user:
+    #         dict_user = self.user.serialize_user()
+    #     return {
+    #         "name": self.name,
+    #         "path_to_file": self.path_to_file,
+    #         "user": dict_user
+    #     }
 
     def serialize(self):
         return {
-            "id": self.id,
-            "username": self.username,
-            "email": self.email,
-            "favorites": self.favorites,
-            # do not serialize the password, its a security breach
-        }
+                    "id": self.id,
+                    "username": self.username,
+                    "email": self.email,
+                    "favorites": [favorite.serialize() for favorite in self.favorites],
+            }    
 
 class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,7 +61,7 @@ class Favorite(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     entity_id = db.Column(db.Integer, unique=True, nullable=False)
     url = db.Column(db.String(200), unique=False, nullable=False)
-    username = db.Column(db.String(120), db.ForeignKey(user.name), unique=False, nullable=False)
+    username = db.Column(db.String(120), db.ForeignKey('user.username'), unique=False, nullable=False)
 
     def __repr__(self):
         return '<Favorite %r>' % self.name
